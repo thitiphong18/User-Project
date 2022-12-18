743d142e2d2ee875f2ac5ff4f039d77e56b799b8
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-19 00:23:25 +0700

    Update 4

---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-card width="400px">
+        <v-card-title primary-title> Login </v-card-title>
+        <v-card-text> Form </v-card-text>
+        <v-card-actions>
+          <v-btn color="success">Login</v-btn>
+          <v-btn color="error">Clear</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-main>
+  </v-app>
+</template>
