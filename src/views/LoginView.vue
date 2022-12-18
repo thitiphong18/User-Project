34fe0cf88bf62b0f3fd31b30bdb09e1dcbb63751
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-19 00:53:37 +0700

    Update 5

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,11 +2,15 @@
 
 <template>
   <v-app>
-    <v-main>
-      <v-card width="400px">
-        <v-card-title primary-title> Login </v-card-title>
-        <v-card-text> Form </v-card-text>
-        <v-card-actions>
+    <v-main class="bg-blue-grey-darken-2 pa-16">
+      <v-card width="400px" class="mx-auto">
+        <v-card-title primary-title class="text-center"> Login </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Login Name"></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions class="justify-center">
           <v-btn color="success">Login</v-btn>
           <v-btn color="error">Clear</v-btn>
         </v-card-actions>
