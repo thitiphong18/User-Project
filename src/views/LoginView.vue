d9a72b5e90be26233e7fbafaec0f66ad00c89c3d
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-19 02:19:21 +0700

    Update 8

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import type { VForm } from "vuetify/components";
 const loginName = ref("");
 const password = ref("");
+const valid = ref(true);
+const form = ref<InstanceType<typeof VForm> | null>(null);
+const login = async () => {
+  const { valid } = await form.value!.validate();
+  if (valid) {
+    console.log("Form Succes");
+  }
+};
+const reset = () => {
+  form.value?.reset();
+};
 </script>
 
 <template>
   <v-app>
     <v-main class="bg-blue-grey-darken-2 pa-16">
       <v-card width="400px" class="mx-auto">
-        <v-card-title primary-title class="text-center"> Login </v-card-title>
+        <v-card-title
+          primary-title
+          class="text-center text-h6 font-weight-bold"
+        >
+          Login
+        </v-card-title>
         <v-card-text>
-          <v-form>
+          <v-form ref="form" v-model="valid">
             <v-text-field
               label="Login Name"
               v-model="loginName"
@@ -20,6 +37,7 @@ const password = ref("");
                   v.length >= 4 ||
                   'Name must be more than or equal 4 characters',
               ]"
+              required
             ></v-text-field>
             <v-text-field
               label="Password"
@@ -31,16 +49,13 @@ const password = ref("");
                   v.length >= 8 ||
                   'Password must be more than or equal 8 characters',
               ]"
+              required
             ></v-text-field>
           </v-form>
-          <pre>
-            {{ loginName }}
-            {{ password }}
-          </pre>
         </v-card-text>
         <v-card-actions class="justify-center">
-          <v-btn color="success">Login</v-btn>
-          <v-btn color="error">Clear</v-btn>
+          <v-btn color="success" @click="login">Login</v-btn>
+          <v-btn color="error" @click="reset">Clear</v-btn>
         </v-card-actions>
       </v-card>
     </v-main>
