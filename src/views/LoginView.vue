b0e56b18e957cfc2ef6d310659d9d9c2bc673cbd
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-19 01:02:56 +0700

    Update 6

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const loginName = ref("");
+const password = ref("");
+</script>
 
 <template>
   <v-app>
@@ -7,8 +11,17 @@
         <v-card-title primary-title class="text-center"> Login </v-card-title>
         <v-card-text>
           <v-form>
-            <v-text-field label="Login Name"></v-text-field>
+            <v-text-field label="Login Name" v-model="loginName"></v-text-field>
+            <v-text-field
+              label="Password"
+              type="password"
+              v-model="password"
+            ></v-text-field>
           </v-form>
+          <pre>
+            {{ loginName }}
+            {{ password }}
+          </pre>
         </v-card-text>
         <v-card-actions class="justify-center">
           <v-btn color="success">Login</v-btn>
