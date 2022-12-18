077284e80881d8870483b1500b9959ca5742faf6
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-18 23:48:32 +0700

    Update 1

---
+++ src/views/MainView.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :prepend-icon="
+          theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+        "
+        @click="onClick"
+        >Toggle Theme</v-btn
+      >
+    </v-app-bar>
+    <v-navigation-drawer expand-on-hover rail permanent>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Sandra Adams"
+          subtitle="sandra_a88@gmailcom"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item
+          prepend-icon="mdi-folder"
+          title="My Files"
+          value="myfiles"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-multiple"
+          title="Shared with me"
+          value="shared"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-star"
+          title="Starred"
+          value="starred"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container>Content area</v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const theme = ref("light");
+
+function onClick() {
+  theme.value = theme.value === "light" ? "dark" : "light";
+}
+</script>
