75d9c0a0d5c64fb8be5e549eb6f115b3554403ef
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-19 12:55:13 +0700

    Update 9

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import MainView from "./views/MainView.vue";
 import LoginVIew from "./views/LoginView.vue";
+import { ref } from "vue";
+const loginName = ref("");
+const isLogin = computed(() => {
+  //loginName is not empty
+  return loginName.value !== "";
+});
 </script>
 
 <template>
-  <LoginVIew />
+  <LoginVIew v-if="!loginName" />
+  <MainView v-if="isLogin" />
 </template>
 
 <style scoped></style>
